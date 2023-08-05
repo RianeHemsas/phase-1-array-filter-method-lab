f046a578f12d6c3c800823589b203bd96f2af23e
Hemsas Riane <rayan.hemsas@gmail.com>
2023-08-05 08:35:58 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+
+function findMatching (array,name){
+   let newArray= array.filter(element=> element.toLowerCase()===name.toLowerCase());
+return newArray;
+}
+console.log( findMatching(drivers,"Bobby"));
+function fuzzyMatch(array,name){
+   let firstLetter= array.filter(element=>element[0]===name[0]);
+
+return firstLetter;
+    
+}
+console.log(fuzzyMatch(drivers,"Sally"));
+const driversArray= [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+
+function matchName(array, paramater){
+    let newArray= array.filter(element=> element.name===paramater);
+    return newArray;
+
+}
+console.log( matchName(driversArray,"Bobby"));
+
